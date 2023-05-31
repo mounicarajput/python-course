3f55fe79531ebee8132f2031633a89361c065089
mounicarajput <rajputmonika953@gmail.com>
2023-05-31 23:33:32 +0530

    add python function

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-# Function for writing "hello world"
+# 1.  Function for writing "hello world"
 
 def hello():
     print("hello world")
 
 hello()
+
+# 2. Write a Python program to print the following string in a specific format (see the output).
+# Sample String: "Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are!"
+
+print("Twinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high, Like a diamond in the sky. \n\t\tTwinkle, twinkle, little star, \n\tHow I wonder what you are!"
+)
+
+#3. Write a Python program to find out what version of Python you are using.
+
+import sys
+print("python version")
+print(sys.version)
+
+# 4. Write a Python program to display the current date and time.
