b01d7fa129aca128294d90454f037d97db892c3c
raghav689 <vr9803093@gmail.com>
2024-08-09 13:31:03 +0530

    list

---
+++ list.py
@@ -0,0 +1,11 @@
+movies = []
+
+mov1 = input("enter 1st movie")
+mov2 = input("enter 2nd movie")
+mov3 = input("enter 3rd movie")
+
+movies.append(mov1)
+movies.append(mov2)
+movies.append(mov3)
+
+print(movies)git
